a716f3f985dc04b2cc485fbbd3df881e93525693
Geralt <littlecuong@gmail.com>
2024-09-19 10:38:54 +0700

    Add new route

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
+@app.get("/new-route")
+def new_api():
+    return {"message": "Hello, This is new API!"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
